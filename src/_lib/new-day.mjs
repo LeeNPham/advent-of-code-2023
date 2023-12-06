ac2358242c1b5f8c636f36e62cd4f18662d3e75c
Lee Pham <leenpham@gmail.com>
2023-12-05 20:45:39 -0800

    put new day back to original functions

--- src/_lib/new-day.mjs
+++ src/_lib/new-day.mjs
@@ -5,20 +5,20 @@ const baseDirectory = './src/day-00'
 const newDirectory = './src/day-' + day
 const extension = 'mjs'
 
-// await $`cp -r ${baseDirectory} ${newDirectory}`
-await fs.copy(baseDirectory, newDirectory)
+await $`cp -r ${baseDirectory} ${newDirectory}`
+// await fs.copy(baseDirectory, newDirectory)
 
 await Promise.all([
-  fs.move(
-    `${newDirectory}/day-00.${extension}`,
-    `${newDirectory}/day-${day}.${extension}`,
-  ),
-  fs.move(
-    `${newDirectory}/day-00.test.${extension}`,
-    `${newDirectory}/day-${day}.test.${extension}`,
-  ),
-  // $`mv ${newDirectory}/day-00.${extension} ${newDirectory}/day-${day}.${extension}`,
-  // $`mv ${newDirectory}/day-00.test.${extension} ${newDirectory}/day-${day}.test.${extension}`,
+  // fs.move(
+  //   `${newDirectory}/day-00.${extension}`,
+  //   `${newDirectory}/day-${day}.${extension}`,
+  // ),
+  // fs.move(
+  //   `${newDirectory}/day-00.test.${extension}`,
+  //   `${newDirectory}/day-${day}.test.${extension}`,
+  // ),
+  $`mv ${newDirectory}/day-00.${extension} ${newDirectory}/day-${day}.${extension}`,
+  $`mv ${newDirectory}/day-00.test.${extension} ${newDirectory}/day-${day}.test.${extension}`,
 ])
 
 cd(newDirectory)
