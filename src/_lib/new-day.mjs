a8d33dbb055d547ad32811b792bf1bb30bc4b984
Lee Pham <leenpham@gmail.com>
2023-12-06 13:20:43 -0800

    created day 5

--- src/_lib/new-day.mjs
+++ src/_lib/new-day.mjs
@@ -6,24 +6,15 @@ const newDirectory = './src/day-' + day
 const extension = 'mjs'
 
 await $`cp -r ${baseDirectory} ${newDirectory}`
-// await fs.copy(baseDirectory, newDirectory)
 
 await Promise.all([
-  // fs.move(
-  //   `${newDirectory}/day-00.${extension}`,
-  //   `${newDirectory}/day-${day}.${extension}`,
-  // ),
-  // fs.move(
-  //   `${newDirectory}/day-00.test.${extension}`,
-  //   `${newDirectory}/day-${day}.test.${extension}`,
-  // ),
   $`mv ${newDirectory}/day-00.${extension} ${newDirectory}/day-${day}.${extension}`,
   $`mv ${newDirectory}/day-00.test.${extension} ${newDirectory}/day-${day}.test.${extension}`,
 ])
 
 cd(newDirectory)
-await $`npx rexreplace 'Day 00' 'Day ${day}/' *`
-await $`npx rexreplace 'Day00' 'Day${day}' *`
-await $`npx rexreplace 'day-00' 'day-${day}' *`
+await $`sed -i 's/Day 00/Day ${day}/g' *`
+await $`sed -i 's/Day00/Day${day}/g' *`
+await $`sed -i 's/day-00/day-${day}/g' *`
 
 console.log(`Finished creating: Day ${day} at ${newDirectory}`)
