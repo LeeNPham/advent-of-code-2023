77790d8fe5977fc35c301146fca25e0241c9d3b0
Lee Pham <leenpham@gmail.com>
2023-12-06 22:29:42 -0800

    update back to problem 1

--- src/day-05/day-05.mjs
+++ src/day-05/day-05.mjs
@@ -5,43 +5,43 @@ class Day05 extends Problem {
     super(inputFileName)
   }
 
-  // solvePart1() {
-  //   function createMaps(data) {
-  //     const maps = {}
-  //     let currentMapName = ''
-  //     for (const line of data) {
-  //       if (line.endsWith('map:')) {
-  //         currentMapName = line.slice(0, -5) // Remove ' map:' from the title
-  //         maps[currentMapName] = []
-  //       } else if (line.trim()) {
-  //         maps[currentMapName].push(line.split(' ').map(Number))
-  //       }
-  //     }
-  //     return maps
-  //   }
+  solvePart1() {
+    function createMaps(data) {
+      const maps = {}
+      let currentMapName = ''
+      for (const line of data) {
+        if (line.endsWith('map:')) {
+          currentMapName = line.slice(0, -5) // Remove ' map:' from the title
+          maps[currentMapName] = []
+        } else if (line.trim()) {
+          maps[currentMapName].push(line.split(' ').map(Number))
+        }
+      }
+      return maps
+    }
 
-  //   let seedLocations = []
-  //   const listOfSeeds = this.lines[0].split(': ')[1].split(' ')
-  //   const mappingList = createMaps(this.lines.slice(2, this.lines.length))
-  //   for (let seed of listOfSeeds) {
-  //     for (let key in mappingList) {
-  //       for (let value of mappingList[key]) {
-  //         if (
-  //           parseInt(seed) >= value[1] &&
-  //           parseInt(seed) <= value[1] + value[2]
-  //         ) {
-  //           seed = parseInt(seed) + (value[0] - value[1])
-  //           break
-  //         }
-  //       }
-  //     }
-  //     seedLocations.push(seed)
-  //   }
+    let seedLocations = []
+    const listOfSeeds = this.lines[0].split(': ')[1].split(' ')
+    const mappingList = createMaps(this.lines.slice(2, this.lines.length))
+    for (let seed of listOfSeeds) {
+      for (let key in mappingList) {
+        for (let value of mappingList[key]) {
+          if (
+            parseInt(seed) >= value[1] &&
+            parseInt(seed) <= value[1] + value[2]
+          ) {
+            seed = parseInt(seed) + (value[0] - value[1])
+            break
+          }
+        }
+      }
+      seedLocations.push(seed)
+    }
 
-  //   let smallestValue = Math.min(...seedLocations.map(Number))
-  //   // console.log('The smallest value is:', smallestValue)
-  //   return smallestValue
-  // }
+    let smallestValue = Math.min(...seedLocations.map(Number))
+    // console.log('The smallest value is:', smallestValue)
+    return smallestValue
+  }
 
   solvePart2() {
     function createMaps(data) {
--- src/day-05/day-05.test.mjs
+++ src/day-05/day-05.test.mjs
@@ -8,9 +8,9 @@ const __dirname = dirname(__filename)
 describe('Day 05', () => {
   describe('example.txt', () => {
     const problem = new Problem(__dirname + '/example.txt')
-    // it('solves part 1', () => {
-    //   expect(problem.solvePart1()).toBe(35)
-    // })
+    it('solves part 1', () => {
+      expect(problem.solvePart1()).toBe(35)
+    })
 
     // it('solves part 2', () => {
     //   expect(problem.solvePart2()).toBe(46)
@@ -20,12 +20,12 @@ describe('Day 05', () => {
   describe('input.txt', () => {
     const problem = new Problem(__dirname + '/input.txt')
 
-    // it('solves part 1', () => {
-    //   expect(problem.solvePart1()).toBe(579439039)
-    // })
-
-    it('solves part 2', () => {
-      expect(problem.solvePart2()).toBe(456789)
+    it('solves part 1', () => {
+      expect(problem.solvePart1()).toBe(579439039)
     })
+
+    // it('solves part 2', () => {
+    //   expect(problem.solvePart2()).toBe(456789)
+    // })
   })
 })
