5f6c5587589d9e7f0ac5eec20bc6af2103f37155
Lee Pham <leenpham@gmail.com>
2023-12-14 17:17:00 -0800

    Starting on part 2 day 8

--- src/day-08/day-08.mjs
+++ src/day-08/day-08.mjs
@@ -41,7 +41,38 @@ class Day08 extends Problem {
   }
 
   solvePart2() {
-    return parseInt(this.solvePart1())
+    let networkMap = {}
+    const directions = this.lines[0]
+    console.log({ directions })
+
+    const direction = (i) => {
+      let effectiveIndex = i % directions.length
+      return directions[effectiveIndex]
+    }
+
+    const network = this.lines.splice(2, this.lines.length - 3)
+    network.map(
+      (str) =>
+        (networkMap[str.slice(0, 3)] = [str.slice(7, 10), str.slice(12, 15)]),
+    )
+    console.log({ networkMap })
+
+    let i = 0
+    let foundTarget = false
+    let currentPlace = 'AAA'
+    let currentTarget = 'ZZZ'
+    // use a recursive function where it loops through the network map, with a breakCase where target = 'ZZZ'
+    while (foundTarget == false) {
+      if (currentPlace !== currentTarget) {
+        // change value of currentPlace based on the i (which is either the left or right value which is the next key)
+        currentPlace = networkMap[currentPlace][direction(i) == 'L' ? 0 : 1]
+        i++
+      } else {
+        foundTarget = true
+      }
+    }
+    console.log(i)
+    return i
   }
 }
 
